a9ac53f705dd77e0a474f3c4a06bacb8eddbef51
WaddyLeonvil <54086752+WaddyLeonvil@users.noreply.github.com>
2022-03-24 18:36:08 -0400

    Created App

---
+++ src/components/Navbar.js
@@ -0,0 +1,20 @@
+import { Search } from '@material-ui/icons';
+import React from 'react';
+import './Navbar.css';
+
+export default function Navbar() {
+  return (
+    <div className='navbar-container'>
+        <div className="navbar-wrapper">
+            <div className="navbar-left">
+                <span className="language">EN</span>
+                <div className="search-container">
+                    <Search />
+                </div>
+            </div>
+            <div className="navbar-center">Center</div>
+            <div className="navbar-right">Right</div>
+        </div>
+    </div>
+  )
+}

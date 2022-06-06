b8902e828c3aeeda3020bd9621f1086070a73d29
WaddyLeonvil <54086752+WaddyLeonvil@users.noreply.github.com>
2022-06-06 17:51:36 -0400

    Added to product list page

--- src/pages/ProductList.js
+++ src/pages/ProductList.js
@@ -16,10 +16,36 @@ export default function ProductList() {
             </h1>
             <div className="filter-container">
                 <div className="filter">
-                    Filter
+                    <span className="filter-text">
+                        Filter Products:
+                    </span>
+                    <select name="" id="" className="product-list-select">
+                        <option value="color" disabled selected>Color</option>
+                        <option value="white">White</option>
+                        <option value="black">Black</option>
+                        <option value="red">Red</option>
+                        <option value="blue">Blue</option>
+                        <option value="yellow">Yellow</option>
+                        <option value="green">Green</option>
+                    </select>
+                    <select name="" id="" className="product-list-select">
+                        <option value="size" disabled selected>Size</option>
+                        <option value="XS">XS</option>
+                        <option value="S">S</option>
+                        <option value="M">M</option>
+                        <option value="L">L</option>
+                        <option value="XL">XL</option>
+                    </select>
                 </div>
                 <div className="filter">
-                    Filter2
+                    <span className="filter-text">
+                        Sort Products:
+                    </span>
+                    <select name="" id="" className="product-list-select">
+                        <option value="newest" selected>Newest</option>
+                        <option value="price-a">Price (asc)</option>
+                        <option value="price-d">Price (desc)</option>
+                    </select>
                 </div>
             </div>
             <Products />
